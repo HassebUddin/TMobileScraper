eefd776dd08ccd0ac3327d131e9f4dc0ba3c14b8
haseebuddin <haseeb.uddin@techno-communications.com>
2026-08-13 02:10:12 +0500

    remove space for testing

--- TMobileScraper.Worker/Data/TechnoDevContext.cs
+++ TMobileScraper.Worker/Data/TechnoDevContext.cs
@@ -6,6 +6,5 @@ namespace TMobileScraper.Data;
 public sealed class TechnoDevContext : DbContext
 {
     public TechnoDevContext(DbContextOptions<TechnoDevContext> options) : base(options) { }
-
     public DbSet<ScrapingWebsite> ScrapingWebsites => Set<ScrapingWebsite>();
 }
